29eec8e16da92a30bd07237b0d22667530554811
Jack-Z1858 <wonderfulawful@gmail.com>
2026-04-23 19:25:42 +0800

    before thread

--- src/detect/main.cpp
+++ src/detect/main.cpp
@@ -1,41 +1,16 @@
 //
 // Created by ubuntu2404 0n 2026/4/1
 //
-#include "opencv2/opencv.hpp"
 #include "yolov8.hpp"
+#include <memory>
+#include <opencv2/core/utility.hpp>  // cv::glob
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <filesystem>
 #include <chrono>
 
-namespace fs = ghc::filesystem;
-
-const std::vector<std::string> CLASS_NAMES = {
-    "person",         "bicycle",    "car",           "motorcycle",    "airplane",     "bus",           "train",
-    "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",    "parking meter", "bench",
-    "bird",           "cat",        "dog",           "horse",         "sheep",        "cow",           "elephant",
-    "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",     "handbag",       "tie",
-    "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball",  "kite",          "baseball bat",
-    "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",       "wine glass",    "cup",
-    "fork",           "knife",      "spoon",         "bowl",          "banana",       "apple",         "sandwich",
-    "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",        "donut",         "cake",
-    "chair",          "couch",      "potted plant",  "bed",           "dining table", "toilet",        "tv",
-    "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",   "microwave",     "oven",
-    "toaster",        "sink",       "refrigerator",  "book",          "clock",        "vase",          "scissors",
-    "teddy bear",     "hair drier", "toothbrush"};
-
-const std::vector<std::vector<unsigned int>> COLORS = {
-    {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
-    {162, 20, 47},   {76, 76, 76},    {153, 153, 153}, {255, 0, 0},     {255, 128, 0},   {191, 191, 0},
-    {0, 255, 0},     {0, 0, 255},     {170, 0, 255},   {85, 85, 0},     {85, 170, 0},    {85, 255, 0},
-    {170, 85, 0},    {170, 170, 0},   {170, 255, 0},   {255, 85, 0},    {255, 170, 0},   {255, 255, 0},
-    {0, 85, 128},    {0, 170, 128},   {0, 255, 128},   {85, 0, 128},    {85, 85, 128},   {85, 170, 128},
-    {85, 255, 128},  {170, 0, 128},   {170, 85, 128},  {170, 170, 128}, {170, 255, 128}, {255, 0, 128},
-    {255, 85, 128},  {255, 170, 128}, {255, 255, 128}, {0, 85, 255},    {0, 170, 255},   {0, 255, 255},
-    {85, 0, 255},    {85, 85, 255},   {85, 170, 255},  {85, 255, 255},  {170, 0, 255},   {170, 85, 255},
-    {170, 170, 255}, {170, 255, 255}, {255, 0, 255},   {255, 85, 255},  {255, 170, 255}, {85, 0, 0},
-    {128, 0, 0},     {170, 0, 0},     {212, 0, 0},     {255, 0, 0},     {0, 43, 0},      {0, 85, 0},
-    {0, 128, 0},     {0, 170, 0},     {0, 212, 0},     {0, 255, 0},     {0, 0, 43},      {0, 0, 85},
-    {0, 0, 128},     {0, 0, 170},     {0, 0, 212},     {0, 0, 255},     {0, 0, 0},       {36, 36, 36},
-    {73, 73, 73},    {109, 109, 109}, {146, 146, 146}, {182, 182, 182}, {219, 219, 219}, {0, 114, 189},
-    {80, 183, 189},  {128, 128, 0}};
+namespace fs = std::filesystem;
 
 // argument count： 命令行参数数量
 // argument vector：存储所有命令行参数的字符串数组
@@ -54,7 +29,7 @@ int main(int argc, char** argv) {
     std::vector<std::string> imagePathList;
     bool                     isVideo{false};
     
-    auto yolov8 = new YOLOv8(engine_file_path);
+    std::unique_ptr<YOLOv8> yolov8 = std::make_unique<YOLOv8>(engine_file_path);
     yolov8->make_pipe(true);
 
     if (fs::exists(path)) {
@@ -74,9 +49,9 @@ int main(int argc, char** argv) {
         cv::glob(path.string() + "/*.jpg", imagePathList);
     }
 
-    cv::Mat             res, image;
-    cv::Size            size = cv::Size{640, 640};
-    std::vector<Object> objs;
+    cv::Mat                  res, image;
+    cv::Size                 size = cv::Size{640, 640};
+    std::vector<det::Object> objs;
 
     cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
 
@@ -120,6 +95,5 @@ int main(int argc, char** argv) {
         }
     }
     cv::destroyAllWindows();
-    delete yolov8;
     return 0;
 }
